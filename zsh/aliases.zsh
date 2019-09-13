d323833ff898e4578bc02060d61578be52f1c43f
SanduNicu <sandu.nicu94@gmail.com>
2019-09-13 15:58:33 +0300

    ohmyzsh git aliases fix

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# ALIASES
+
+alias dot="cd $HOME/.dotfiles && nvim ."
+
+alias v="nvim"
+alias vim="nvim"
+alias r="ranger"
+alias p="sudo pacman"
+alias y="yay"
+alias ka="killall"
+alias :q="exit"
+alias mk=make
+
+alias reload!='. ~/.zshrc'
+alias cls='clear' # Good 'ol Clear Screen command
+alias tn="tmux new -s"
+alias ta="tmux a -t"
