9cd9f30e386c752b00ab55642c4b67a4e0376f77
SanduNicu <sandu.nicu94@gmail.com>
2019-09-18 23:03:30 +0300

    fixed aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,17 +2,17 @@
 
 alias dot="cd $HOME/.dotfiles && nvim ."
 
-alias v="nvim"
-alias vim="nvim"
-alias r="ranger"
-alias p="sudo pacman"
-alias y="yay"
-alias ka="killall"
-alias :q="exit"
-alias mk=make
-alias pro= sudo pacman -Rs $(pacman -Qqtd)
+alias v='nvim'
+alias vim='nvim'
+alias r='ranger'
+alias p='sudo pacman'
+alias y='yay'
+alias ka='killall'
+alias :q='exit'
+alias mk='make'
+alias pclean='sudo pacman -Rs $(pacman -Qqtd)'
 
 alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
-alias tn="tmux new -s"
-alias ta="tmux a -t"
+alias tn='tmux new -s'
+alias ta='tmux a -t'
